491edc5b79cba7790514bca297599bcdbfcede50
Alex <Alex@DESKTOP-0VBSDS2>
2020-11-09 23:55:51 -0400

    agregue file ejemplo01

---
+++ SDatosPersonales/PDatosPersonales/principal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Apellidos: Cruz Fernandez" << endl;
+	cout << "Nombres: Alex Juvenal" << endl;
+	cout << "Carreras: Ing. de Sistemas / CIC" << endl;
+	cout << "CU: 35-4027  /  111-85" << endl;
+	cout << "Grupo T: 1" << endl;
+	cout << "Grupo L: 2" << endl;
+	system("pause");
+	return 0;
+}
